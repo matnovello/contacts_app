45b8b10d9cb6b06350b34a35652a7aff2b05d11f
Mat <mvello66@gmail.com>
2021-02-23 19:43:54 -0500

    adds faker gem, new contact data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+contact = Contact.new({ first_name: "John",
+                        last_name: "smith", email: "jsmith@gmail.com", phone_number: "888-888-0000" })
+contact.save
+
+random_contacts = []
+contact = Contact.new({ first_name: Faker::Name.unique.name })
+i = 0
+
+while i < 100
+  random_contacts << contact
+  contact.save
+  i += 1
+end
+
+funny_name = Contact.new({first_name:})
